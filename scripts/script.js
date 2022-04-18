95f4030c43cce3f344df786046ee6505f7b42ed9
ZzZorger <yalex007@mail.ru>
2022-04-18 17:47:12 +0300

    main_after_review

--- scripts/script.js
+++ scripts/script.js
@@ -2,43 +2,42 @@ const popupOpenButton = document.querySelector('.profile__edit-button');
 const popup = document.querySelector('.popup');
 const popupCloseButton = popup.querySelector('.popup__close');
 const popupSaveButton = popup.querySelector('.popup__save');
-const cardButton = document.querySelector('.card__button');
 let profileName = document.querySelector('.profile__name');
 let profileJob = document.querySelector('.profile__job');
-document.querySelector('.popup__name').value = profileName.textContent;
-document.querySelector('.popup__job').value = profileJob.textContent;
+popup.querySelector('.popup__input_type_name').value = profileName.textContent;
+popup.querySelector('.popup__input_type_job').value = profileJob.textContent;
 
 // Фукнция открытия popup
 function popupOpenToggle() {
   popup.classList.toggle('popup_is-opened');
+  popup.querySelector('.popup__input_type_name').value = profileName.textContent;
+  popup.querySelector('.popup__input_type_job').value = profileJob.textContent;
 }
 
 // Фукнция закрытия popup
 function popupOpenToggle() {
   popup.classList.toggle('popup_is-opened');
-  document.querySelector('.popup__name').value = profileName.textContent;
-  document.querySelector('.popup__job').value = profileJob.textContent;
 }
 
-// Функция сохранения имени и работы профиля
+// Функция сохранения данных при нажатии на кнопку
 function popupSaveButtonToggle() {
-  profileName.innerHTML = document.querySelector('.popup__name').value;
-  profileJob.innerHTML = document.querySelector('.popup__job').value;
+  profileName.textContent = document.querySelector('.popup__input_type_name').value;
+  profileJob.textContent = document.querySelector('.popup__input_type_job').value;
   popup.classList.toggle('popup_is-opened');
 }
 
-popupSaveButton.addEventListener('click', popupSaveButtonToggle);
+// Сохранение данных при нажатии клавиши Enter
+document.querySelector('.popup__input_type_name').addEventListener('keydown', function (e) {
+  if (e.keyCode === 13) {
+    popupSaveButtonToggle();
+  }
+});
+document.querySelector('.popup__input_type_job').addEventListener('keydown', function (e) {
+  if (e.keyCode === 13) {
+    popupSaveButtonToggle();
+  }
+});
+
 popupOpenButton.addEventListener('click', popupOpenToggle);
 popupCloseButton.addEventListener('click', popupOpenToggle);
-
-// Сохранение данных при нажатии клавиши Enter
-document.querySelector('.popup__name').addEventListener('keydown', function(e) {
-  if (e.keyCode === 13) {
-    popupSaveButtonToggle();
-  }
-});
-document.querySelector('.popup__job').addEventListener('keydown', function(e) {
-  if (e.keyCode === 13) {
-    popupSaveButtonToggle();
-  }
-});
+popupSaveButton.addEventListener('click', popupSaveButtonToggle);
