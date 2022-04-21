86daf9d2e59a3cd8cf88fad2d79b2f379cae7311
ZzZorger <yalex007@mail.ru>
2022-04-21 22:17:38 +0300

    main_after_review_v2

--- scripts/script.js
+++ scripts/script.js
@@ -1,43 +1,38 @@
 const popupOpenButton = document.querySelector('.profile__edit-button');
 const popup = document.querySelector('.popup');
-const popupCloseButton = popup.querySelector('.popup__close');
-const popupSaveButton = popup.querySelector('.popup__save');
-let profileName = document.querySelector('.profile__name');
-let profileJob = document.querySelector('.profile__job');
-popup.querySelector('.popup__input_type_name').value = profileName.textContent;
-popup.querySelector('.popup__input_type_job').value = profileJob.textContent;
+const popupCloseButton = popup.querySelector('.popup__close-button');
+const popupSaveButton = popup.querySelector('.popup__save-button');
+const profileName = document.querySelector('.profile__name');
+const profileJob = document.querySelector('.profile__job');
+const popupInputName = document.querySelector('.popup__input_type_name');
+const popupInputJob = document.querySelector('.popup__input_type_job');
+const popupInputNameValue = popup.querySelector('.popup__input_type_name');
+const popupInputJobValue = popup.querySelector('.popup__input_type_job');
+const popupForm = popup.querySelector('.popup__form');
 
 // Фукнция открытия popup
-function popupOpenToggle() {
-  popup.classList.toggle('popup_is-opened');
-  popup.querySelector('.popup__input_type_name').value = profileName.textContent;
-  popup.querySelector('.popup__input_type_job').value = profileJob.textContent;
+function popupOpen() {
+  popupInputNameValue.value = profileName.textContent;
+  popupInputJobValue.value = profileJob.textContent;
+  popup.classList.add('popup_is-opened');
 }
 
 // Фукнция закрытия popup
-function popupOpenToggle() {
-  popup.classList.toggle('popup_is-opened');
+function popupClose() {
+  popup.classList.remove('popup_is-opened');
 }
 
 // Функция сохранения данных при нажатии на кнопку
-function popupSaveButtonToggle() {
-  profileName.textContent = document.querySelector('.popup__input_type_name').value;
-  profileJob.textContent = document.querySelector('.popup__input_type_job').value;
-  popup.classList.toggle('popup_is-opened');
+function popupSubmitHandler() {
+  profileName.textContent = popupInputName.value;
+  profileJob.textContent = popupInputJob.value;
+  popupClose();
 }
 
+popupOpenButton.addEventListener('click', popupOpen);
+popupCloseButton.addEventListener('click', popupClose);
 // Сохранение данных при нажатии клавиши Enter
-document.querySelector('.popup__input_type_name').addEventListener('keydown', function (e) {
-  if (e.keyCode === 13) {
-    popupSaveButtonToggle();
-  }
+popupForm.addEventListener('submit', function(evt) {
+  evt.preventDefault();
+  popupSubmitHandler();
 });
-document.querySelector('.popup__input_type_job').addEventListener('keydown', function (e) {
-  if (e.keyCode === 13) {
-    popupSaveButtonToggle();
-  }
-});
-
-popupOpenButton.addEventListener('click', popupOpenToggle);
-popupCloseButton.addEventListener('click', popupOpenToggle);
-popupSaveButton.addEventListener('click', popupSaveButtonToggle);
