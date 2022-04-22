29a503e21559be4a1aa807be4009db82e771b8fa
ZzZorger <yalex007@mail.ru>
2022-04-22 14:34:51 +0300

    main_after_review_v3

--- scripts/script.js
+++ scripts/script.js
@@ -1,19 +1,16 @@
 const popupOpenButton = document.querySelector('.profile__edit-button');
 const popup = document.querySelector('.popup');
 const popupCloseButton = popup.querySelector('.popup__close-button');
-const popupSaveButton = popup.querySelector('.popup__save-button');
 const profileName = document.querySelector('.profile__name');
 const profileJob = document.querySelector('.profile__job');
 const popupInputName = document.querySelector('.popup__input_type_name');
 const popupInputJob = document.querySelector('.popup__input_type_job');
-const popupInputNameValue = popup.querySelector('.popup__input_type_name');
-const popupInputJobValue = popup.querySelector('.popup__input_type_job');
 const popupForm = popup.querySelector('.popup__form');
 
 // Фукнция открытия popup
 function popupOpen() {
-  popupInputNameValue.value = profileName.textContent;
-  popupInputJobValue.value = profileJob.textContent;
+  popupInputName.value = profileName.textContent;
+  popupInputJob.value = profileJob.textContent;
   popup.classList.add('popup_is-opened');
 }
 
@@ -23,7 +20,8 @@ function popupClose() {
 }
 
 // Функция сохранения данных при нажатии на кнопку
-function popupSubmitHandler() {
+function popupSubmitHandler(evt) {
+  evt.preventDefault();
   profileName.textContent = popupInputName.value;
   profileJob.textContent = popupInputJob.value;
   popupClose();
@@ -31,8 +29,4 @@ function popupSubmitHandler() {
 
 popupOpenButton.addEventListener('click', popupOpen);
 popupCloseButton.addEventListener('click', popupClose);
-// Сохранение данных при нажатии клавиши Enter
-popupForm.addEventListener('submit', function(evt) {
-  evt.preventDefault();
-  popupSubmitHandler();
-});
+popupForm.addEventListener('submit', popupSubmitHandler);
