fa5f2afb4f6ddb053842144870765c98a122499f
Alexandr Yakushkin <jakushkinn@yandex.ru>
2022-06-24 16:59:24 +0300

    UserInfo class done

--- scripts/index.js
+++ scripts/index.js
@@ -2,7 +2,6 @@ import Card from './components/Card.js';
 import FormValidator from './components/FormValidator.js';
 import Section from './components/Section.js';
 import Popup from './components/Popup.js';
-// import PopupWithImage from './components/PopupWithImage.js';
 import PopupWithForm from './components/PopupWithForm.js';
 import UserInfo from './components/UserInfo.js';
 import {
@@ -19,8 +18,6 @@ import {
   profileName,
   popupProfilePlace,
   profileJob,
-  popupCardName,
-  popupCardPlace,
   profilePopupClass,
   cardPopupClass
 }
